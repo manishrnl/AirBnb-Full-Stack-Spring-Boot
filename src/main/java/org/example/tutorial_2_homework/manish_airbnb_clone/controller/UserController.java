d55ab6f5dcff1f060ea25d5784543c2b411eef12
MANISH <manishrajrnl@gmail.com>
2025-12-26 16:33:35 +0530

    Part 8

--- src/main/java/org/example/tutorial_2_homework/manish_airbnb_clone/controller/UserController.java
+++ src/main/java/org/example/tutorial_2_homework/manish_airbnb_clone/controller/UserController.java
@@ -3,6 +3,7 @@ package org.example.tutorial_2_homework.manish_airbnb_clone.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.tutorial_2_homework.manish_airbnb_clone.dto.BookingDto;
 import org.example.tutorial_2_homework.manish_airbnb_clone.dto.ProfileUpdateRequestDto;
+import org.example.tutorial_2_homework.manish_airbnb_clone.dto.UserDto;
 import org.example.tutorial_2_homework.manish_airbnb_clone.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @PutMapping("/profile")
+    @PatchMapping("/profile")
     public ResponseEntity<Void> updateProfile(@RequestBody ProfileUpdateRequestDto profileUpdateRequestDto) {
         userService.updateProfile(profileUpdateRequestDto);
         return ResponseEntity.noContent().build();
@@ -25,5 +26,11 @@ public class UserController {
     @GetMapping("/myBookings")
     public ResponseEntity<List<BookingDto>> getMyBookings() {
         return ResponseEntity.ok(userService.getMyBookings());
+    }
+
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserDto> getMyProfiles(){
+        return ResponseEntity.ok(userService.getMyProfiles());
     }
 }
--- src/main/java/org/example/tutorial_2_homework/manish_airbnb_clone/service/UserService.java
+++ src/main/java/org/example/tutorial_2_homework/manish_airbnb_clone/service/UserService.java
@@ -3,7 +3,10 @@ package org.example.tutorial_2_homework.manish_airbnb_clone.service;
 import lombok.RequiredArgsConstructor;
 import org.example.tutorial_2_homework.manish_airbnb_clone.dto.BookingDto;
 import org.example.tutorial_2_homework.manish_airbnb_clone.dto.ProfileUpdateRequestDto;
+import org.example.tutorial_2_homework.manish_airbnb_clone.dto.UserDto;
+import org.example.tutorial_2_homework.manish_airbnb_clone.entity.UserEntity;
 import org.example.tutorial_2_homework.manish_airbnb_clone.entity.enums.Gender;
+import org.example.tutorial_2_homework.manish_airbnb_clone.exception.ResourceNotFoundException;
 import org.example.tutorial_2_homework.manish_airbnb_clone.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,6 +17,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import static org.example.tutorial_2_homework.manish_airbnb_clone.util.AppUtils.getCurrentUser;
 
 @Service
 @RequiredArgsConstructor
@@ -40,9 +45,28 @@ public class UserService implements UserDetailsService {
     }
 
     public void updateProfile(ProfileUpdateRequestDto profileDto) {
-        String name = profileDto.getName();
+        UserEntity userEntity = getCurrentUser();
+
         LocalDate dateOfBirth = profileDto.getDateOfBirth();
+        String name = profileDto.getName();
         Gender gender = profileDto.getGender();
 
+        if (name != null) userEntity.setName(name);
+        if (gender != null) userEntity.setGender(gender);
+        if (dateOfBirth != null) userEntity.setDateOfBirth(dateOfBirth);
+
+
+        userRepository.save(userEntity);
+
+
+    }
+
+    public UserDto getMyProfiles() {
+        UserEntity userEntity = getCurrentUser();
+        Long userId = userEntity.getId();
+        UserEntity getProfile =
+                userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("We could not find users with id " + userId));
+
+        return modelMapper.map(getProfile, UserDto.class);
     }
 }
